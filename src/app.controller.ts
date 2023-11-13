beed6392d92f0929f6877bfdc052da0f896a04f5
ducngo123456 <135568147+duc-ngo-saltovn@users.noreply.github.com>
2023-11-13 17:27:22 +0700

    get duration of file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 
+const { getAudioDurationInSeconds } = require('get-audio-duration');
+
 @Controller()
 export class AppController {
   @Get('/health')
   healthCheck() {
+    getAudioDurationInSeconds('music1.mp3').then((duration) => {
+      console.log(duration)
+    });
     return {
       message: 'Service is alive',
       status: 200,
